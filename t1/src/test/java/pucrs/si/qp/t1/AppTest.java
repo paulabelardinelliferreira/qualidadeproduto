af6c1f867fc84e8d31013781ce9cb4065ebd5063
paulabelardinelliferreira <54337189+paulabelardinelliferreira@users.noreply.github.com>
2020-05-02 21:42:26 -0300

    Primeiro commit

---
+++ t1/src/test/java/pucrs/si/qp/t1/AppTest.java
@@ -0,0 +1,17 @@
+package pucrs.si.qp.t1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
